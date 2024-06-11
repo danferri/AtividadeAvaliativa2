09152bdd0b754c6f8a7b3335783ffef98c9420bd
Daniel Ferri <89649179+danferri@users.noreply.github.com>
2024-06-11 13:54:00 -0300

    tirar comentários e fazer clientes de teste

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -7,27 +7,32 @@ import { Employee } from '../models/employee.model';
 @Injectable({
   providedIn: 'root'
 })
+
 export class DataService {
 
   constructor() { }
 
   getCustomers(): Observable<Customer[]> {
     return of([
-      new Customer(1, 1, 'João', 'Silva', 'joao.silva@example.com', true),
-
-    ]);
-  }
+      new Customer(1, 1, 'Dan', 'Nameless', 'dan@dan.com.br', true),
+      new Customer(2, 2, 'João', 'Keilo', 'keilo@keilo.com.br', true),
+      new Customer(3, 3, 'TonyTony', 'Chopper', 'tony@tont.com.br', true)  
+    ]); 
+  }  
+  
 
   getAddresses(): Observable<Address[]> {
     return of([
-      new Address(1, 'Rua Principal', 123, '12345-678', '123-456-7890', 'Cidade Exemplo', 'Centro'),
+      new Address(1, 'Love Street', 123, '12345-678', '123-456-7890', 'Sanca City', 'Centro'),
 
     ]);
   }
 
+  
+
   getEmployees(): Observable<Employee[]> {
     return of([
-      new Employee(1, 1, 1, 'Maria', 'Oliveira', 'maria.oliveira@example.com', true, 'senha123'),
+      new Employee(1, 1, 1, 'Maria', 'Maria', 'maria@maria.com.br', true, 'senha123'),
 
     ]);
   }
